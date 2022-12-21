012bff352e69fc768899b7859828bf06770aee08
Yojit Shinde <yojitshinde@gmail.com>
2022-12-22 00:14:07 +0530

    DB Configuration

--- Django/DjangoAPI/DjangoAPI/settings.py
+++ Django/DjangoAPI/DjangoAPI/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'DjangoAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FYPDB',
+        'USER': 'root',
+        'PASSWORD': 'Yojit@17',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
