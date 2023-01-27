97a4947591bdf345026c2e695dfefca1e5902fb6
Yojit Shinde <yojitshinde@gmail.com>
2023-01-27 19:25:16 +0530

    general homepage

--- Angular/app_frontend/src/app/app-routing.module.ts
+++ Angular/app_frontend/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { RegHomeComponent } from './reg-home/reg-home.component';
 
 
 const routes: Routes = [
   //{ path: 'recruiter', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: RegHomeComponent},
   { path: 'recruiter', component: RegistrationPageComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Angular/app_frontend/src/app/app.module.ts
+++ Angular/app_frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { SharedService } from './shared.service';
 
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegHomeComponent } from './reg-home/reg-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationPageComponent
+    RegistrationPageComponent,
+    RegHomeComponent
   ],
   imports: [
     BrowserModule,
