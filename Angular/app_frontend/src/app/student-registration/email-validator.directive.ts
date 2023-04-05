d7187fcafb9419140436bb5da3eda766bb94dbe1
Ankit Prakash <ankitjalaja1@gmail.com>
2023-04-05 10:48:38 +0530

    regex/validation applied on registration forms and email validators

---
+++ Angular/app_frontend/src/app/student-registration/email-validator.directive.ts
@@ -0,0 +1,41 @@
+import { Directive } from '@angular/core';
+import { NG_VALIDATORS, AbstractControl, Validator, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function emailValidator(): ValidatorFn {
+
+  const EMAIL_REGEXP = /^[a-z\.]+@xaviers\.edu\.in$/;
+
+  return (control: AbstractControl): ValidationErrors | null => {
+    const isValid = EMAIL_REGEXP.test(control.value);
+
+    if (isValid) {
+      return null;
+    } else {
+      return {
+        emailValidator: {
+          valid: false,
+        },
+      };
+    }
+  };
+
+}
+
+@Directive({
+  selector: '[appEmailValidator]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: EmailValidatorDirective,
+    multi: true,
+  }],
+})
+export class EmailValidatorDirective implements Validator {
+
+  constructor() {
+  }
+
+  public validate(control: AbstractControl): ValidationErrors | null {
+    return emailValidator()(control);
+  }
+
+}
